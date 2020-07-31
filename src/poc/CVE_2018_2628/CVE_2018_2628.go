8828959e82ca246100c645b8d5c482c8caebdb1a
chw <896350736@qq.com>
2020-07-31 11:33:29 +0800

    add details

--- src/poc/CVE_2018_2628/CVE_2018_2628.go
+++ src/poc/CVE_2018_2628/CVE_2018_2628.go
@@ -54,13 +54,15 @@ func sendEvilObjData(conn net.Conn, data string) []byte {
 	conn.Write(payload2)
 	var res []byte
 	buf := make([]byte, 4096)
-	for {
+	count := 0
+	for count < 10 {
 		n, err := conn.Read(buf)
 		if err != nil {
 			break
 		}
 		res = append(res, buf[:n]...)
 		time.Sleep(100 * time.Millisecond)
+		count += 1
 	}
 	return res
 }
